358140535d6f131bcc3f7650fdffe39705b6c9d5
C00MAR <moureau.marc78@gmail.com>
2025-11-19 14:50:25 +0100

    :sparkles: Add Web PWA support :sparkles:

--- lib/presentation/pages/catalog_page.dart
+++ lib/presentation/pages/catalog_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import '../viewmodels/catalog_viewmodel.dart';
 
 class CatalogPage extends StatelessWidget {
@@ -14,6 +15,25 @@ class CatalogPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          if (kIsWeb)
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Installer ShopFlutter'),
+                    content: const Text('Ajoutez cette application a votre ecran d\'accueil pour un acces rapide'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Text('Installer PWA'),
+            ),
           Consumer<CatalogViewModel>(
             builder: (context, viewModel, _) {
               return Column(
