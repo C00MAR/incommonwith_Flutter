eafffe2b9745f897bd8c7f2402e0d51ed93188fc
C00MAR <moureau.marc78@gmail.com>
2025-11-20 16:45:04 +0100

    :fire: Fixing Vercel Deployment :fire:

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -23,29 +23,51 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'YOUR_WEB_API_KEY',
-    appId: 'YOUR_WEB_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    apiKey: String.fromEnvironment(
+      'FIREBASE_WEB_API_KEY',
+      defaultValue: '',
+    ),
+    appId: String.fromEnvironment(
+      'FIREBASE_WEB_APP_ID',
+      defaultValue: '',
+    ),
+    messagingSenderId: String.fromEnvironment(
+      'FIREBASE_MESSAGING_SENDER_ID',
+      defaultValue: '',
+    ),
+    projectId: String.fromEnvironment(
+      'FIREBASE_PROJECT_ID',
+      defaultValue: '',
+    ),
+    authDomain: String.fromEnvironment(
+      'FIREBASE_AUTH_DOMAIN',
+      defaultValue: '',
+    ),
+    storageBucket: String.fromEnvironment(
+      'FIREBASE_STORAGE_BUCKET',
+      defaultValue: '',
+    ),
+    measurementId: String.fromEnvironment(
+      'FIREBASE_MEASUREMENT_ID',
+      defaultValue: '',
+    ),
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'YOUR_ANDROID_API_KEY',
-    appId: 'YOUR_ANDROID_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    apiKey: 'AIzaSyDLpzIdiy33vjYw5JufYLq2__nHPssGtrQ',
+    appId: '1:83803930192:android:c7708a66ec3a1e3d94db55',
+    messagingSenderId: '83803930192',
+    projectId: 'incommonwith-flutter',
+    storageBucket: 'incommonwith-flutter.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'YOUR_IOS_API_KEY',
-    appId: 'YOUR_IOS_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    iosBundleId: 'com.example.incommonwithFlutter',
+    apiKey: 'AIzaSyBMu9K9t8tr4_Z9J19zoMCh4lgxBTxJpS4',
+    appId: '1:83803930192:ios:9f8cfc22d9d7791c94db55',
+    messagingSenderId: '83803930192',
+    projectId: 'incommonwith-flutter',
+    storageBucket: 'incommonwith-flutter.firebasestorage.app',
+    iosBundleId: 'com.coomar.flutterIIM',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
